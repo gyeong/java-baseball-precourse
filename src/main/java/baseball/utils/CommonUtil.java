93c4f5ac2a4871767a093ce26539af625475c1b7
gyeong yoon <gyeongyoon@gmail.com>
2021-10-05 11:05:10 +0900

    feat(common): 공통 문자열 및 함수 추가

---
+++ src/main/java/baseball/utils/CommonUtil.java
@@ -0,0 +1,10 @@
+package baseball.utils;
+
+public class CommonUtil {
+    public static final String GAME_NUMBER_ZERO = "0";
+    public static final String GAME_NUMBER_MIN = "1";
+    public static final String GAME_NUMBER_MAX = "9";
+    
+    public static final String GAME_INPUT_LENGTH = "3";
+    
+}
---
+++ src/main/java/baseball/utils/StringUtil.java
@@ -0,0 +1,14 @@
+package baseball.utils;
+
+public class StringUtil {
+    
+    public static boolean isNumber(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+    
+}
