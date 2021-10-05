a37d5fa65b6a5ae42d342a0dbe97f25c05efbad8
gyeong yoon <gyeongyoon@gmail.com>
2021-10-05 13:53:45 +0900

    feat(GamePresenter): GamePresenter 도메인 기능 추가
    GamePresenter 도메인 기능 추가
    - 종료 여부 데이터 validation 체크
    - 종료 여부 체크

--- src/main/java/baseball/domain/GamePresenter.java
+++ src/main/java/baseball/domain/GamePresenter.java
@@ -4,20 +4,21 @@ import baseball.utils.CommonUtil;
 import baseball.utils.StringUtil;
 
 public class GamePresenter {
-    public final String FLAG_RESTART = "1";
-    public final String FLAG_TERMINATE = "2";
-
+    private final String GAME_RESTART = "1";
+    private final String GAME_TERMINATE = "2";
+    
     private final String ASK_PICK_NUMBER = "숫자를 입력해주세요 : ";
     private final String ASK_NEW_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private final String SUCCESS_MSG = "3개의 숫자를 모두 맞히셨습니다! 게임 끝";
 
-    private final String GAME_STRIKE = "스트라이크 ";
-    private final String GAME_BALL = "볼";
-    private final String GAME_NOTHING = "낫싱";
+    private final String RESULT_STRIKE = "스트라이크 ";
+    private final String RESULT_BALL = "볼";
+    private final String RESULT_NOTHING = "낫싱";
 
     private final String ERROR_HEAD = "[ERROR] ";
     private final String ERROR_INPUT_MESSAGE = "3자리 숫자를 입력해주세요.";
     private final String ERROR_INVALID_NUMBER_MESSAGE = "1부터 9까지의 숫자만 입력해주세요.";
+    private final String ERROR_INVALID_TERM_MESSAGE = "1(재시작) 혹은 2(종료) 입력만 가능합니다..";
 
     /**
      * 숫자 입력 안내 메시지 출력
@@ -25,7 +26,7 @@ public class GamePresenter {
     public void printIntroMsg() {
         System.out.print(ASK_PICK_NUMBER);
     }
-
+    
     /**
      * 사용자 게임 숫자 입력값 유효성 체크
      * 
@@ -37,6 +38,19 @@ public class GamePresenter {
         }
 
         return true;
+    } 
+    
+    /**
+     * 사용자 게임 종료/재시작 입력값 유효성 체크
+     * 
+     * @param input
+     */
+    public boolean checkTermValidation(String input) {
+        if (!input.equals(GAME_RESTART) || !input.equals(GAME_TERMINATE) ) {
+            printErrorMsg(ERROR_INVALID_TERM_MESSAGE);
+            return false;
+        }
+        return true;
     }
 
     /**
@@ -44,10 +58,10 @@ public class GamePresenter {
      */
     public void printResultMsg(int strikeCount, int ballCount) {
         if (strikeCount == 0 && ballCount == 0) {
-            System.out.println(GAME_NOTHING);
+            System.out.println(RESULT_NOTHING);
             return;
         }
-
+        
         System.out.println(makeResultMsg(strikeCount, ballCount));
     }
 
@@ -61,10 +75,10 @@ public class GamePresenter {
         String resultMsg = "";
 
         if (strikeCount > 0) {
-            resultMsg += strikeCount + GAME_STRIKE;
+            resultMsg += strikeCount + RESULT_STRIKE;
         }
         if (ballCount > 0) {
-            resultMsg += ballCount + GAME_BALL;
+            resultMsg += ballCount + RESULT_BALL;
         }
 
         return resultMsg;
@@ -99,7 +113,7 @@ public class GamePresenter {
      * @param input
      */
     public boolean checkLength(String input) {
-        if (input.length() != 3) {
+        if (input.length() != CommonUtil.GAME_INPUT_LENGTH) {
             printErrorMsg(ERROR_INPUT_MESSAGE);
             return false;
         }
@@ -122,5 +136,18 @@ public class GamePresenter {
             return false;
         }
         return true;
+    }
+    
+    /**
+     * 게임 종료 여부 확인
+     * 
+     * @param input
+     */
+    public boolean isEnd(String input) {
+        if (input.equals(GAME_TERMINATE)) {
+            return true;
+        }
+        
+        return false;
     }
 }
